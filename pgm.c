fdca5039d417eee32f523f29f732cf028c0656b3
Ubuntu <saitharunbairi123@gmail.com>
2022-05-02 05:49:58 +0000

    I am pushing all my files

---
+++ pgm.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+printf(" Hello! My first C program for Github");
+return 0;
+}
---
+++ pgm1.java
@@ -0,0 +1,5 @@
+class HelloJava{
+public static void main(string args[]){
+system.out.Println("My first Java Program for Github");
+}
+}
