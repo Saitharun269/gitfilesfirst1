9706e865f8d8e22c8e7caef1d4b095694ea36611
Saitharun269 <64146785+Saitharun269@users.noreply.github.com>
2022-05-04 11:59:54 +0530

    Update pgm1.java

--- pgm1.java
+++ pgm1.java
@@ -1,5 +1,5 @@
 class HelloJava{
 public static void main(string args[]){
-system.out.Println("My first Java Program for Github");
+system.out.Println("My first Java Program for Github_Jenkins auto build");
 }
 }
